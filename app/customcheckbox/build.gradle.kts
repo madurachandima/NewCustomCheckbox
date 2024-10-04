6ff200156201413d22ec06cd366903dd983e7c54
MaduraDiasTreinetic <madurachandima6@gmail.com>
2024-10-04 13:50:03 +0530

    Initial commit

---
+++ app/customcheckbox/build.gradle.kts
@@ -0,0 +1,65 @@
+import com.android.build.gradle.internal.scope.publishBuildArtifacts
+
+plugins {
+    alias(libs.plugins.android.library)
+    id("maven-publish")
+
+}
+
+android {
+    namespace = "com.madura.customcheckbox"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                groupId = "com.github.philipplackner"
+                artifactId = "customcheckbox"
+                version = "1.0"
+                afterEvaluate {
+                    from(components["release"])
+                }
+            }
+        }
+    }
+}
+
